fe7ceb41ffaa4ca36bac21966f01d392511ffc2a
Joseph Cooper <cooper.joseph@gmail.com>
2023-04-20 16:23:17 +0100

    feat: Work on multiple files inplace

--- src/main.rs
+++ src/main.rs
@@ -1,79 +1,87 @@
 use clap::Parser;
 use color_eyre::eyre;
-use color_eyre::eyre::eyre;
+use color_eyre::eyre::{ensure, eyre, WrapErr};
 use freedesktop_entry_parser::Entry;
 use itertools::Itertools;
 use mediatype::names::{APPLICATION, TEXT};
 use mediatype::{media_type, MediaType, MediaTypeError, Name};
+use std::fs;
+use std::io::Write;
 use std::path::PathBuf;
 
-static DESKTOP_ENTRY_SECTION: &str = "Desktop Entry";
-
-static MIME_TYPES_KEY: &str = "MimeType";
-
 fn main() -> eyre::Result<()> {
+    color_eyre::install()?;
     let args = Cli::parse();
-    let desktop_file = Entry::parse_file(&args.input_file)?;
 
-    if desktop_file.sections().count() > 1 {
-        eprintln!("Can't process files with more than one section");
-        std::process::exit(1)
-    }
+    for input_file in &args.input_files {
+        let desktop_file = Entry::parse_file(input_file)
+            .wrap_err_with(|| format!("Error parsing file {}", input_file.to_string_lossy()))?;
 
-    if !desktop_file.has_section(DESKTOP_ENTRY_SECTION) {
-        eprintln!(
-            "No [Desktop Entry] section found in file {}",
-            args.input_file.to_string_lossy()
-        );
-        std::process::exit(2)
+        let buf = process_file(&desktop_file)
+            .wrap_err_with(|| format!("Error processing file {}", input_file.to_string_lossy()))?;
+
+        fs::write(input_file, &buf)
+            .wrap_err_with(|| format!("Error writing to file {}", input_file.to_string_lossy()))?;
     }
+    Ok(())
+}
+
+fn process_file(desktop_file: &Entry) -> eyre::Result<Vec<u8>> {
+    ensure!(
+        desktop_file.sections().count() == 1,
+        "Can't process files with more than one section"
+    );
+
+    ensure!(
+        desktop_file.has_section(&DESKTOP_ENTRY_SECTION),
+        "No [Desktop Entry] section found in file"
+    );
 
     let section = desktop_file.section(DESKTOP_ENTRY_SECTION);
 
     let mime_types = section
         .attr(MIME_TYPES_KEY)
-        .ok_or_else(|| {
-            eyre!(
-                "Can't find MimeTypes entry in file {}",
-                args.input_file.to_string_lossy()
-            )
-        })?
+        .ok_or_else(|| eyre!("Can't find MimeTypes entry in file"))?
         .split_terminator(";")
         .map(MediaType::parse)
         .collect::<Result<Vec<_>, MediaTypeError>>()?;
 
-    let disallowed_media_types = [
-        media_type!(TEXT / HTML),
-        media_type!(TEXT / RTF),
-        media_type!(APPLICATION / vnd::MS_WORD_DOCUMENT_MACRO_ENABLED_12),
-        media_type!(TEXT / PLAIN),
-        media_type!(APPLICATION / vnd::OPENXMLFORMATS_OFFICEDOCUMENT_WORDPROCESSINGML_DOCUMENT),
-        MediaType::new(TEXT, X_MARKDOWN),
-        media_type!(APPLICATION / vnd::OASIS_OPENDOCUMENT_TEXT),
-        media_type!(APPLICATION / PDF),
-        media_type!(APPLICATION / XHTML + XML),
-        MediaType::new(APPLICATION, X_RUBY),
-    ];
-
     let filtered_mime_types = mime_types
-        .into_iter()
-        .filter(|m| !disallowed_media_types.contains(m))
+        .iter()
+        .filter(|&m| DISALLOWED_MEDIA_TYPES.iter().any(|e| e != m))
         .join(";");
 
-    println!("[{}]", section.name());
+    let mut buf = Vec::new();
+
+    writeln!(buf, "[{}]", section.name())?;
     for key in KEYS {
         if let Some(val) = section.attr(key) {
-            println!("{}={}", key, val);
+            writeln!(buf, "{key}={val}")?;
         }
     }
 
-    println!("{MIME_TYPES_KEY}={filtered_mime_types}");
-
-    Ok(())
+    writeln!(buf, "{MIME_TYPES_KEY}={filtered_mime_types}")?;
+    Ok(buf)
 }
 
-static X_RUBY: Name = Name::new_unchecked("x-ruby");
-static X_MARKDOWN: Name = Name::new_unchecked("x-markdown");
+const DISALLOWED_MEDIA_TYPES: [MediaType; 10] = [
+    media_type!(TEXT / HTML),
+    media_type!(TEXT / RTF),
+    media_type!(APPLICATION / vnd::MS_WORD_DOCUMENT_MACRO_ENABLED_12),
+    media_type!(TEXT / PLAIN),
+    media_type!(APPLICATION / vnd::OPENXMLFORMATS_OFFICEDOCUMENT_WORDPROCESSINGML_DOCUMENT),
+    MediaType::new(TEXT, X_MARKDOWN),
+    media_type!(APPLICATION / vnd::OASIS_OPENDOCUMENT_TEXT),
+    media_type!(APPLICATION / PDF),
+    media_type!(APPLICATION / XHTML + XML),
+    MediaType::new(APPLICATION, X_RUBY),
+];
+
+const X_RUBY: Name = Name::new_unchecked("x-ruby");
+const X_MARKDOWN: Name = Name::new_unchecked("x-markdown");
+
+static MIME_TYPES_KEY: &str = "MimeType";
+static DESKTOP_ENTRY_SECTION: &str = "Desktop Entry";
 
 static KEYS: [&str; 10] = [
     "Version",
@@ -91,6 +99,7 @@ static KEYS: [&str; 10] = [
 #[derive(Debug, Parser)]
 #[command(author, version, about)]
 struct Cli {
-    /// .desktop file to process
-    input_file: PathBuf,
+    /// .desktop files to process
+    #[arg(required = true)]
+    input_files: Vec<PathBuf>,
 }
